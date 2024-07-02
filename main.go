dfa63c9125f020d15e92741adda9e6231b2bd7d0
Emily Huang <emilyhua@amazon.com>
2024-07-02 20:49:17 +0000

    adding ListPods function

--- main.go
+++ main.go
@@ -1,32 +1,33 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "path/filepath"
-    "k8s.io/client-go/kubernetes"
-    "k8s.io/client-go/tools/clientcmd"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-    userHomeDir, err := os.UserHomeDir()
-    if err != nil {
-        fmt.Printf("error getting user home dir: %v\n", err)
-        os.Exit(1)
-    }
-    kubeConfigPath := filepath.Join(userHomeDir, ".kube", "config")
-    fmt.Printf("Using kubeconfig: %s\n", kubeConfigPath)
+	userHomeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Printf("error getting user home dir: %v\n", err)
+		os.Exit(1)
+	}
+	kubeConfigPath := filepath.Join(userHomeDir, ".kube", "config")
+	fmt.Printf("Using kubeconfig: %s\n", kubeConfigPath)
 
-    kubeConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
-    if err != nil {
-        fmt.Printf("Error getting kubernetes config: %v\n", err)
-        os.Exit(1)
-    }
+	kubeConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+	if err != nil {
+		fmt.Printf("Error getting kubernetes config: %v\n", err)
+		os.Exit(1)
+	}
 
-    clientset, err := kubernetes.NewForConfig(kubeConfig)
+	clientset, err := kubernetes.NewForConfig(kubeConfig)
 
-    if err != nil {
-        fmt.Printf("error getting kubernetes config: %v\n", err)
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Printf("error getting kubernetes config: %v\n", err)
+		os.Exit(1)
+	}
 }
--- pkg/utils.go
+++ pkg/utils.go
@@ -1,1 +1,19 @@
 package k8s
+
+import (
+	"context"
+	"fmt"
+
+	"k8s.io/client-go/kubernetes"
+)
+
+// ListPods lists Kubernetes Pods by namespace(s)
+func ListPods(namespace string, client kubernetes.Interface) (*v1.PodList, error) {
+	fmt.Println("Get Kubernetes Pods")
+	pods, err := client.CoreV1().Pods(namespace).List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		err = fmt.Errorf("error getting pods: %v\n", err)
+		return nil, err
+	}
+	return pods, nil
+}
